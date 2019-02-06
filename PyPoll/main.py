07b4bfec8dd2d658ed7b1a099c82d6027310a2ea
Joey Ashcroft <ashcroft.joey@gmail.com>
2019-02-06 01:29:29 -0700

    Add files via upload
    updated new pandas functions to replace old methods/numpy functions

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,63 +1,43 @@
 import csv
 import os
-import numpy as np
+import pandas as pd
 
 #IMPORT DATA####################################################
+
 #read the csv
-path_to_file=os.path.join(".","election_data.csv")
+path=os.path.join(".","election_data.csv")
 
-#create lists to fill
-voterid=[]
-county=[]
-candidate=[]
-
-with open(path_to_file,newline="",encoding="utf8") as csvfile:
-    data=csv.reader(csvfile, delimiter=',')
-
-    for row in data:
-        #write to lists
-        voterid.append(row[0])
-        county.append(row[1])
-        candidate.append(row[2])
-
-#delete headers in each list
-del voterid[0]
-del county[0]
-del candidate[0]
+#import data as csv
+data=pd.read_csv(path)
 
 #MAIN###########################################################
 
 #total number of votes cast
-total_votes=len(voterid)
+total_votes=data.shape[0]                 
 
 #array candidate names
-names=np.unique(candidate)
-
-#convert list to numpy array
-candidate_array=np.asarray(candidate)
-
-#initialize list of number of votes for each candidate
-candidate_num=[]
+names=data["Candidate"].unique()
 
 print("ELECTION RESULTS:")
 print(f"Total Votes: {total_votes}")
 print("---------------------")
 
-#for loop to output a summary of each candidate in the list
+#number of votes for each candidate
+candidate_num=data["Candidate"].value_counts()
+
+#for loop to print out summary statistics
 for i in range(len(names)):
-    candidate_num.append(len(np.where(candidate_array == names[i])[0]))
     print(f"{names[i]}: {round(candidate_num[i]/total_votes*100,2)}% ({candidate_num[i]})")
 
 print("---------------------")
 #print winner
-print(f"Winner: {names[candidate_num.index(np.max(candidate_num))]}")
-
+print(f"Winner: {candidate_num.idxmax()}")
 
 #EXPORT###########################################################
 
 #lists to export
 votes=["Total Votes",total_votes,"","",""]
-winner=["Winner",names[candidate_num.index(np.max(candidate_num))],"","",""]
+winner=["Winner",candidate_num.idxmax(),"","",""]
 blank=["","","","",""]
 line1=["Candidate Name"]
 line2=["Vote Percentage"]
@@ -70,6 +50,7 @@ for i in range(len(names)):
 
 #combine all lines to 1
 all_lines=zip(votes,winner,blank,line1,line2,line3)
+
 
 # save the output file path
 output_file = os.path.join("election_results.txt")
